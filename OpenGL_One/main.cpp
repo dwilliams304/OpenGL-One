424cf850ee4df7b721e34b499d311cc6db34b26a
Davis W <d.williams0434@gmail.com>
2025-07-31 00:16:11 -0400

    draw a point, wow!

--- OpenGL_One/main.cpp
+++ OpenGL_One/main.cpp
@@ -1,21 +1,59 @@
 #include <stdio.h>
 #include <glew.h>
 #include <glut.h>
+#include <freeglut.h>
+
+GLuint VBO;
+
+struct Vector3f {
+	float x;
+	float y;
+	float z;
+
+	Vector3f() {}
+
+	Vector3f(float _x, float _y, float _z) {
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+
+	Vector3f(const float* pFloat) {
+		x = pFloat[0];
+		y = pFloat[1];
+		z = pFloat[2];
+	}
+
+	Vector3f(float f) {
+		x = y = z = f;
+	}
+};
 
 
 static void RenderSceneCallback() 
 {
-	static GLclampf c = 0.0f;
-
-	glClearColor(c * 2, c / 2, 0, c);
-
-	c += 1.0f / 256.f;
-	if (c >= 1.0f) {
-		c = 0.0f;
-	}
 	glClear(GL_COLOR_BUFFER_BIT);
-	glutPostRedisplay();
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawArrays(GL_POINTS, 0, 1);
+
+	glDisableVertexAttribArray(0);
+
 	glutSwapBuffers();
+}
+
+static void CreateVertexBuffer() {
+	Vector3f Vertices[1];
+
+	Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 }
 
 int main(int argc, char* argv[]) 
@@ -25,19 +63,25 @@ int main(int argc, char* argv[])
 
 	int width = 1028;
 	int height = 760;
-
 	glutInitWindowSize(width, height);
 
 	int x = 200;
 	int y = 100;
-
 	glutInitWindowPosition(x, y);
 	int win = glutCreateWindow("Open GL-One");
-
 	printf("window id: %d\n", win);
+	
+	GLenum res = glewInit();
+	if (res != GLEW_OK) {
+		fprintf(stderr, "Error: %s\n", glewGetErrorString(res));
+		return 1;
+	}
+
 
 	GLclampf Red = 1.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
 	glClearColor(Red, Green, Blue, Alpha);
+
+	CreateVertexBuffer();
 	
 	glutDisplayFunc(RenderSceneCallback);
 
