48ae2fd29eb9a67067a043b3898c7f7380ef7259
Davis W <d.williams0434@gmail.com>
2025-07-30 23:43:44 -0400

    make screen, change colors, wow

--- OpenGL_One/main.cpp
+++ OpenGL_One/main.cpp
@@ -3,18 +3,45 @@
 #include <glut.h>
 
 
+static void RenderSceneCallback() 
+{
+	static GLclampf c = 0.0f;
+
+	glClearColor(c * 2, c / 2, 0, c);
+
+	c += 1.0f / 256.f;
+	if (c >= 1.0f) {
+		c = 0.0f;
+	}
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutPostRedisplay();
+	glutSwapBuffers();
+}
+
 int main(int argc, char* argv[]) 
 {
-	printf("Hi Open GL!");
-
 	glutInit(&argc, argv);
-	glutCreateWindow("GLEW Test");
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 
-	GLenum err = glewInit();
-	if (GLEW_OK != err) {
-		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-	}
-	fprintf(stdout, "Status: Using Glew %s\n", glewGetString(GLEW_VERSION));
-	getchar();
+	int width = 1028;
+	int height = 760;
+
+	glutInitWindowSize(width, height);
+
+	int x = 200;
+	int y = 100;
+
+	glutInitWindowPosition(x, y);
+	int win = glutCreateWindow("Open GL-One");
+
+	printf("window id: %d\n", win);
+
+	GLclampf Red = 1.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
+	glClearColor(Red, Green, Blue, Alpha);
+	
+	glutDisplayFunc(RenderSceneCallback);
+
+	glutMainLoop();
+
 	return 0;
 }
